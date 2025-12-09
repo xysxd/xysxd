dae8f912e511c687594cf063f61949ca228e6337
xysxd <sfw2647@gmail.com>
2025-12-09 14:57:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const UPSTREAM_URL = "http://23.94.151.110:81"; 
+const UPSTREAM_URL = "http://23.94.151.110:81";
+
 export default {
-    async fetch(request, env, ctx) {
-        const url = new URL(request.url);
-        const newUrl = new URL(url.pathname + url.search, UPSTREAM_URL);
-        const newRequest = new Request(newUrl, request);
-        return fetch(newRequest);
-    }
+  async fetch(request) {
+    const url = new URL(request.url);
+    
+    // 构造新的 URL
+    const newUrl = new URL(url.pathname + url.search, UPSTREAM_URL);
+    
+    // 创建新的请求对象
+    const newRequest = new Request(newUrl, request);
+    
+    // 转发请求并返回响应
+    return fetch(newRequest);
+  }
 };
