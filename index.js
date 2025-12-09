07975b45306f4221bbf720456800bae0af311565
xysxd <sfw2647@gmail.com>
2025-12-09 14:53:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const UPSTREAM_URL = "http://23.94.151.110:8080"; 
-
-export async function onRequest(context) {
-  const request = context.request;
-  const url = new URL(request.url);
-
-  const newUrl = new URL(url.pathname + url.search, UPSTREAM_URL);
-  const newRequest = new Request(newUrl, request);
-
-  return fetch(newRequest);
-}
+const UPSTREAM_URL = "http://23.94.151.110:81"; 
+export default {
+    async fetch(request, env, ctx) {
+        const url = new URL(request.url);
+        const newUrl = new URL(url.pathname + url.search, UPSTREAM_URL);
+        const newRequest = new Request(newUrl, request);
+        return fetch(newRequest);
+    }
+};
