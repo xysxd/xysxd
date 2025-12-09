6dff6ca77409b423569ee3f66079320297c690d9
xysxd <sfw2647@gmail.com>
2025-12-09 14:59:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+// 注意：确保这一行只包含半角字符和英文引号
 const UPSTREAM_URL = "http://23.94.151.110:81";
 
+// 使用标准的 Worker 模块导出格式：export default { fetch }
 export default {
-  async fetch(request) {
+  async fetch(request, env, ctx) { // 这里的 env 和 ctx 参数可以保留
     const url = new URL(request.url);
     
     // 构造新的 URL
